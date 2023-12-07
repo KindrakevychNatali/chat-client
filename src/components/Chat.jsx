d712a8df0037b55a3dfdf4a03b1b12c96be4c8b5
KindrakevychNatali <natali.kindrakevych@gmail.com>
2023-12-07 17:10:51 +0300

    Changing link info

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import EmojiPicker from 'emoji-picker-react';
-import { useLocation, useNavigate } from 'react-router';
+import React from "react";
 import io from "socket.io-client";
-import styles from "../styles/Chat.module.css";
-import Messages from './Messages';
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import EmojiPicker from "emoji-picker-react";
+
 import icon from "../images/emoji.svg";
+import styles from "../styles/Chat.module.css";
+import Messages from "./Messages";
 
-
-const socket = io.connect("https://localhost:5000");
+const socket = io.connect("https://online-chat-socket-fb11.onrender.com");
 
 const Chat = () => {
-
   const { search } = useLocation();
-  const [params, setParams] = useState({ user: "", room: "" });
+  const navigate = useNavigate();
+  const [params, setParams] = useState({ room: "", user: "" });
   const [state, setState] = useState([]);
   const [message, setMessage] = useState("");
   const [isOpen, setOpen] = useState(false);
   const [users, setUsers] = useState(0);
-  const navigate = useNavigate();
 
-  useEffect( () => {
+  useEffect(() => {
     const searchParams = Object.fromEntries(new URLSearchParams(search));
     setParams(searchParams);
     socket.emit("join", searchParams);
@@ -27,77 +28,77 @@ const Chat = () => {
 
   useEffect(() => {
     socket.on("message", ({ data }) => {
-      setState((_state) => ([..._state, data]));
+      setState((_state) => [..._state, data]);
     });
   }, []);
 
   useEffect(() => {
-    socket.on("room", ({ data: { users} }) => {
+    socket.on("room", ({ data: { users } }) => {
       setUsers(users.length);
     });
   }, []);
 
   const leftRoom = () => {
     socket.emit("leftRoom", { params });
-    navigate("/"); ///redirection using useNavigate hook!
+    navigate("/");
   };
 
-  const handleChange = ({ target: { value }}) => {
-     setMessage(value);
-  };
- 
-  const onEmojiClick = ({ emoji }) => setMessage(`${message} ${emoji}`);
+  const handleChange = ({ target: { value } }) => setMessage(value);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if(!message) return; ///if message is empty interrupt function using return!
-    socket.emit("sendMessage", { message, params });
-    setMessage("");///cleaning field message(state)
+    if (!message) return;
 
+    socket.emit("sendMessage", { message, params });
+
+    setMessage("");
   };
+
+  const onEmojiClick = ({ emoji }) => setMessage(`${message} ${emoji}`);
 
   return (
     <div className={styles.wrap}>
-        <div className={styles.header}>
-            <div className={styles.title}>
-                {params.room}
-            </div>
-            <div className={styles.users}>
-                {users} users in this room
-            </div>
-            <button className={styles.left} onClick={leftRoom}>
-                Left room!
-            </button>
-        </div>
-        <div className={styles.messages}>
-          <Messages messages={state} username={params.name}/>
-        </div>
-        <form className={styles.form} onSubmit={handleSubmit}>
-           <div className={styles.input}>
-             <input type="text" 
-                    name="message" 
-                    placeholder='What do you wanna write?'
-                    value={message}
-                    onChange={handleChange}
-                    autoComplete='off'
-                    required
-              />
-          </div>
-          <div className={styles.emoji}>
-            <img src={icon} alt="" onClick={() => setOpen(!isOpen)} />
-           {isOpen && (
-             <div className={styles.emojies}>
-                <EmojiPicker onEmojiClick={onEmojiClick}></EmojiPicker>
-             </div>
-           )}
-           </div>
-           <div className={styles.button}>
-             <input type="submit" value="Send a message" onSubmit={handleSubmit}/>
-           </div>
-        </form>
+      <div className={styles.header}>
+        <div className={styles.title}>{params.room}</div>
+        <div className={styles.users}>{users} users in this room</div>
+        <button className={styles.left} onClick={leftRoom}>
+          Left the room
+        </button>
       </div>
-    );
-  };
 
-export default Chat
+      <div className={styles.messages}>
+        <Messages messages={state} name={params.name} />
+      </div>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <div className={styles.input}>
+          <input
+            type="text"
+            name="message"
+            placeholder="What do you want to say?"
+            value={message}
+            onChange={handleChange}
+            autoComplete="off"
+            required
+          />
+        </div>
+        <div className={styles.emoji}>
+          <img src={icon} alt="" onClick={() => setOpen(!isOpen)} />
+
+          {isOpen && (
+            <div className={styles.emojies}>
+              <EmojiPicker onEmojiClick={onEmojiClick} />
+            </div>
+          )}
+        </div>
+
+        <div className={styles.button}>
+          <input type="submit" onSubmit={handleSubmit} value="Send a message" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Chat;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,66 +1,73 @@
-import React, { useState } from 'react';
-import styles from '../styles/Main.module.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import styles from "../styles/Main.module.css";
 
 const FIELDS = {
-    USERNAME: "name",
-    ROOM: "room",
-}
+  NAME: "name",
+  ROOM: "room",
+};
 
 const Main = () => {
-    const { NAME, ROOM } = FIELDS;
-    const [values, setValues] = useState({ [NAME]: "", [ROOM]: "" });
+  const { NAME, ROOM } = FIELDS;
 
-    const handleChange = ({ target: { value, name } }) => {
-        setValues({...value, [name]: value });
-    };
+  const [values, setValues] = useState({ [NAME]: "", [ROOM]: "" });
 
-    const handleClick = (e) => {
-        const isDisabled = Object.values(values).some((value) => !value);
+  const handleChange = ({ target: { value, name } }) => {
+    setValues({ ...values, [name]: value });
+  };
 
-        if(isDisabled) e.preventDefault();
-    }
+  const handleClick = (e) => {
+    const isDisabled = Object.values(values).some((v) => !v);
+
+    if (isDisabled) e.preventDefault();
+  };
+
   return (
     <div className={styles.wrap}>
-        <div className={styles.container}>
-            <h1 className={styles.heading}>Join</h1>
-            <form className={styles.form}>
-                <div className={styles.group}>
-                    <input type="text" 
-                           name="name" 
-                           placeholder='User name'
-                           value={values[NAME]}
-                           className={styles.input}
-                           onChange={handleChange}
-                           autoComplete='off'
-                           required
-                     />
-                </div>
-                <div className={styles.group}>
-                    <input type="text" 
-                           name="room" 
-                           placeholder='Room'
-                           value={values[ROOM]} 
-                           className={styles.input}
-                           onChange={handleChange}
-                           autoComplete='off'
-                           required
-                     />
-                </div>
-                <Link 
-                    className={styles.group}
-                    onClick={handleClick}
-                    to={`/chat?name=${values[NAME]}&room=${values[ROOM]}`}>
-                    <button type='submit'
-                             className={styles.button}
-                    >
-                        Sign In
-                    </button>
-                </Link>
-            </form>
-        </div>
-    </div>
-  )
-}
+      <div className={styles.container}>
+        <h1 className={styles.heading}>Join</h1>
 
-export default Main
+        <form className={styles.form}>
+          <div className={styles.group}>
+            <input
+              type="text"
+              name="name"
+              value={values[NAME]}
+              placeholder="Username"
+              className={styles.input}
+              onChange={handleChange}
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div className={styles.group}>
+            <input
+              type="text"
+              name="room"
+              placeholder="Room"
+              value={values[ROOM]}
+              className={styles.input}
+              onChange={handleChange}
+              autoComplete="off"
+              required
+            />
+          </div>
+
+          <Link
+            className={styles.group}
+            onClick={handleClick}
+            to={`/chat?name=${values[NAME]}&room=${values[ROOM]}`}
+          >
+            <button type="submit" className={styles.button}>
+              Sign In
+            </button>
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
